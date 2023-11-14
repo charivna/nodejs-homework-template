df3a89c788d6d25507149a4024690cb4b3aa47f5
charivna <kate.dzhurinskaya@gmail.com>
2023-11-14 13:05:34 +0100

    config

---
+++ config.js
@@ -0,0 +1,4 @@
+const DB_HOST =
+  "mongodb+srv://Kate:Katerina123@cluster0.pemqsyf.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+module.exports = { DB_HOST };
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const app = require("./app");
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
+const { DB_HOST } = require("./config");
 
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Kate:Katerina123@cluster0.pemqsyf.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
